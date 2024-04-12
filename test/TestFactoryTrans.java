3273b0df3b12283d00bc6e75f4890b4d654301fd
angel <apineiroandion@danielcastelao.org>
2024-04-12 09:57:50 +0200

    añando DisplayName en test

--- test/TestFactoryTrans.java
+++ test/TestFactoryTrans.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import  org.junit.jupiter.api.Assertions;
@@ -6,20 +7,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TestFactoryTrans {
     @Test
+    @DisplayName("Camion no nulo")
     public void testGetTransporteReturnCamionNotNull() {
         assertNotNull(FactoryTrans.getTransporte(FactoryTrans.CAMION));
     }
     @Test
+    @DisplayName("Bicicleta no nulo")
     public void testGetTransporteReturnBicicletaNotNull() {
         assertNotNull(FactoryTrans.getTransporte(FactoryTrans.BICICLETA));
     }
     //Preguntar por este metodo
     @Test
+    @DisplayName("Tipo Bicileta")
     public void testGetTransporteAssertEqualsBicicletaRetrunTrue() {
         Bicicleta bicicleta = new Bicicleta();
         assertNotEquals(FactoryTrans.getTransporte(FactoryTrans.BICICLETA), bicicleta);
     }
     @Test
+    @DisplayName("Tipo Camion")
     public void testGetTransporteAssertEqualsCamionRetrunTrue() {
         Camion camion = new Camion();
         assertNotEquals(FactoryTrans.getTransporte(FactoryTrans.CAMION), camion);
